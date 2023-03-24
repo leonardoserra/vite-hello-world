b1bcf10d8ae648a7fbbb06e3593e29093d50b2e9
leonardoserra <serra.leonardo92@gmail.com>
2023-03-24 15:16:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AppTitle from './components/AppTitle.vue';
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style scoped></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>La mia prima app con VITE!</h1>
+</template>
+
+<style scoped></style>
