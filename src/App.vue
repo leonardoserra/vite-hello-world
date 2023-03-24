31c69555d8ae96b675562866d26c8f74bbedab70
leonardoserra <serra.leonardo92@gmail.com>
2023-03-24 15:43:32 +0100

    sfondo

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,23 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <AppTitle />
-    <AppHero />
+  <div class="body">
+    <div class="container">
+      <AppTitle />
+      <AppHero />
+    </div>
   </div>
 </template>
 
 <style scoped>
+.body {
+  width: 100%;
+  height: 100%;
+  background-image: url(../public/img1.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
 .container {
   text-align: center;
   width: 600px;
